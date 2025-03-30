06387cf6d00a40d676e613cf3122ca27df4dda08
LordRiccard <106322393+LordRiccard@users.noreply.github.com>
2025-03-29 21:21:02 -0300

    docs: added code documentation and type hints

--- util/extract.py
+++ util/extract.py
@@ -2,16 +2,35 @@ import praw
 import os
 from dotenv import load_dotenv
 import pandas as pd
+from praw.models import Subreddit
 
-def setup_praw ():
+def setup_praw () -> praw.Reddit:
+    """
+    Establishes a connection with reddit.
+    Utilizes client_id, client_secret and user_agent from .env
+
+    returns:
+        praw.Reddit: Instance connected to reddit
+    """
     load_dotenv()
 
     return praw.Reddit(client_id = os.getenv("client_id"),
                        client_secret = os.getenv("client_secret"),
                        user_agent = os.getenv("user_agent"))
 
+def get_data (subreddit: Subreddit) -> pd.DataFrame:
+    """
+    Collect posts from the hot section of a subreddit.
+    The info gathered: Post's author, title,
+    score (thumbs up - thumbs down), url, body
+    (written text), num. of comments, flair and
+    time created
 
-def get_data (subreddit):
+    param:
+        subreddit (Subreddit): The chosen subreddit
+    returns:
+        pd.DataFrame: Data collected
+    """
     list_author = []
     list_title = []
     list_score = []
@@ -44,7 +63,15 @@ def get_data (subreddit):
 
     return pd.DataFrame(data)
 
-def extract (output_path):
+def extract (output_path: str) -> None:
+    """
+    Starts a praw instance with setup_praw() and
+    uses it to extract data with get_data(). At
+    the end saves the data collected on a csv file
+
+    param:
+        output_path (str): String with a file path
+    """
     reddit = setup_praw()
     subreddit = reddit.subreddit('explainlikeimfive')
 
@@ -52,6 +79,6 @@ def extract (output_path):
     df.to_csv(output_path, index=False)
 
 if __name__ == '__main__':
-    output_path = '../output/extracted.csv'
-    extract(output_path)
+    output_file = '../output/extracted.csv'
+    extract(output_file)
     print('Finished extracting')
--- util/load.py
+++ util/load.py
@@ -6,7 +6,12 @@ import os
 import csv
 
 base = declarative_base()
+
 class Posts(base):
+    """
+    Object Relational Mapping used to create the
+    'posts' table inside MySQL database
+    """
     __tablename__ = 'posts'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -20,32 +25,57 @@ class Posts(base):
     created = Column(DateTime)
     keywords = Column(Text)
 
-def get_engine ():
+def get_engine () -> Engine:
+    """
+    Creates an engine instance using
+    "sql_connection_string" from .env.
+    The engine is tied to redditdb
+    returns:
+        Engine: The engine instance
+    """
     load_dotenv()
     return create_engine(os.getenv("sql_connection_string")
                            + 'redditdb')
 
-def create_table (engine):
+def create_table (engine: Engine) -> None:
+    """
+    Creates 'posts' table inside MySQL database
+    param:
+        engine (Engine): The engine linked to a
+                         MySQL database
+    """
     base.metadata.create_all(engine)
 
-def load_from_file (engine, input_path):
-    Session = sessionmaker(bind=engine)
+def insert_from_file (engine: Engine, input_path: str) -> None:
+    """
+    Insert rows from a csv into a database
 
+    param:
+        engine (Engine): The engine used
+        input_path (str): The path to the file
+    """
     with open(input_path, encoding='utf-8', newline='\n') as csv_file:
         csvreader = csv.DictReader(csv_file, quotechar='"')
 
         posts = [Posts(**row) for row in csvreader]
 
+        Session = sessionmaker(bind=engine)
         session = Session()
         session.add_all(posts)
         session.commit()
 
-def load (input_path):
+def load (input_path: str) -> None:
+    """
+    Creates an engine instance with get_engine()
+    Creates the table needed using create_table()
+    Insert the data in the db with insert_from_file()
+    """
     engine = get_engine()
     create_table(engine)
-    load_from_file(engine, input_path)
+    insert_from_file(engine, input_path)
 
 if __name__ == '__main__':
-    input_path = '../output/transformed.csv'
+    input_file = '../output/transformed.csv'
 
-    load(input_path)
+    load(input_file)
+    print('Finished Loading')
